134863323b01d1d13925eb0c58ee0a97928f4dff
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-02-02 11:57:02 +0530

    Added header and footer components

--- pizza-menu-app/src/index.js
+++ pizza-menu-app/src/index.js
@@ -49,11 +49,21 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
       <Pizza />
       <Pizza />
       <Pizza />
+      <Footer />
     </div>
+  );
+}
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {}
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
   );
 }
 
