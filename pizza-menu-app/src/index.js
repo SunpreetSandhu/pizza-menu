9f58ccf12e0e3dbc8c63054cd34064120580b511
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-03-13 17:08:07 -0400

    Conditional rendering with bound

--- pizza-menu-app/src/index.js
+++ pizza-menu-app/src/index.js
@@ -64,14 +64,19 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/* <Pizza
         name="Pizza Spinaci"
         ingredient="Tomato, mozarella, spinach, and ricotta cheese"
@@ -114,7 +119,12 @@ function Footer() {
   console.log(hour);
   return (
     <footer className="footer">
-      {isOpen && <p>We're open until {closeHour}:00. Come visit us!</p>}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us!</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
