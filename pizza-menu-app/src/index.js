51e1ac2f55d0cb5483b372d9a0cee545c5885093
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-03-13 17:35:00 -0400

    Conditional rendering with tenrary

--- pizza-menu-app/src/index.js
+++ pizza-menu-app/src/index.js
@@ -69,12 +69,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu come back later!</p>
       )}
 
       {/* <Pizza
@@ -119,11 +121,13 @@ function Footer() {
   console.log(hour);
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us!</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>We're happy to welcome you between our open hours!</p>
       )}
     </footer>
   );
